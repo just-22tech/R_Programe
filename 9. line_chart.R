3e2c99d5c3201b8a9e361a4af10afee06fa236fb
Parthajit Das <justfuv42@gmail.com>
2025-12-26 19:53:47 +0530

    Add line Chart

---
+++ 9. line_chart.R
@@ -0,0 +1,12 @@
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 4, 6, 8, 10)
+
+plot(x, y, 
+     type = "o", 
+     main = "Line Chart Example", 
+     xlab = "Values of x", 
+     ylab = "Values of y", 
+     col = "blue",
+     lwd = 2) 
+
+grid()
