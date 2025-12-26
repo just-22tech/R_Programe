76a507136f90b9234b0ff99694a7a1ee550ab5c5
Parthajit Das <justfuv42@gmail.com>
2025-12-26 19:40:10 +0530

    Add scatter plot

---
+++ 7. scatter_plot.R
@@ -0,0 +1,9 @@
+x <- c(2, 8, 13, 10, 12, 12, 8)
+y <- c(4, 16, 18, 13, 19, 16, 8)
+
+plot(x, y, 
+     main = "Scatter Plot of X vs Y",
+     xlab = "X Values", 
+     ylab = "Y Values", 
+     pch = 19, 
+     col = "red")
