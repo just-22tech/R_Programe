4df0145567381b762f723ec28fe66ea03f51efe7
Parthajit Das <justfuv42@gmail.com>
2025-12-29 22:11:53 +0530

    Add Question Answer

--- 7. scatter_plot.R
+++ 7. scatter_plot.R
@@ -3,6 +3,9 @@ y <- c(4, 16, 18, 13, 19, 16, 8)
 
 plot(x, y, 
      main = "Scatter Plot of X vs Y",
+     type = "o",
+     pch = 19,
      xlab = "X Values", 
      ylab = "Y Values", 
      col = "red")
+grid()
